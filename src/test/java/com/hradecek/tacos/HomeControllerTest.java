26770847c7db3e01c988136eed4469c9217c784b
hradecek <ivohradek@gmail.com>
2021-01-10 00:48:18 +0100

    Validation 🔥

--- src/test/java/com/hradecek/tacos/HomeControllerTest.java
+++ src/test/java/com/hradecek/tacos/HomeControllerTest.java
@@ -1,11 +1,8 @@
 package com.hradecek.tacos;
 
-import com.hradecek.tacos.controllers.HomeController;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
     @Autowired
